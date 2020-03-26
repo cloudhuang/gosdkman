02fbb1dd683ba64cb21475bdd36c8c3a5a84a84a
Liping Huang <liping.huang@live.com>
2020-03-26 14:15:57 +0800

    delete unsed 3rd packages

--- go.mod
+++ go.mod
@@ -3,10 +3,8 @@ module gosdkman
 go 1.14
 
 require (
-	github.com/cheggaaa/pb/v3 v3.0.4
 	github.com/devfacet/gocmd v3.1.0+incompatible
 	github.com/dustin/go-humanize v1.0.0
 	github.com/gosuri/uilive v0.0.4 // indirect
-	github.com/gosuri/uiprogress v0.0.1
 	gopkg.in/yaml.v2 v2.2.8
 )
--- jdk/jdk_test.go
+++ jdk/jdk_test.go
@@ -2,10 +2,8 @@ package jdk
 
 import (
 	"fmt"
-	"github.com/gosuri/uiprogress"
 	"gosdkman/utils"
 	"testing"
-	"time"
 )
 
 func TestListInstalledVersion(t *testing.T) {
@@ -42,17 +40,4 @@ func TestDownloadRemoteJDKFile(t *testing.T) {
 	//var identifier = "14.27.1-zulu"
 	var jdkFile = "https://cdn.azul.com/zulu/bin/zulu14.27.1-ca-jdk14-win_x64.zip"
 	utils.DownloadFile(jdkFile)
-}
-
-func TestProgressingBar(t *testing.T) {
-	uiprogress.Start()            // start rendering
-	bar := uiprogress.AddBar(100) // Add a new bar
-
-	// optionally, append and prepend completion and elapsed time
-	bar.AppendCompleted()
-	bar.PrependElapsed()
-
-	for bar.Incr() {
-		time.Sleep(time.Millisecond * 20)
-	}
 }
